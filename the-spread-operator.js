bc889ae919a99d111e00fbb695a1833bd82cbaba
YesCoops <coopandmgc@gmail.com>
2022-03-04 13:48:09 +0000

    Added spread operator test

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,29 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("Second Array:", arr2);
 
 // Copying an array
 
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log("Third Array:", arr3);
+console.log("Fourth Array:", arr4);
+
 // Copying an object
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = {...obj1, d: 4};
+let obj3 = {...obj1, b: 5};
+console.log("First Object:", obj1);
+console.log("Second Object:", obj2);
+console.log("Third Object:", obj3);
+
 
 // Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log(arr5);
